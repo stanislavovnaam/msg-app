aade29e7977fb5606a83335a7d484e74af8049d2
stanislavovnaam <rettiw_qiq@mail.ru>
2026-02-11 21:30:50 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 import sqlite3
 import os
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
 app = Flask(__name__)
+app.secret_key = "dev-secret"
 DB_PATH = "data.db"
 
 def init_db():
@@ -34,36 +37,16 @@ def index():
     # POST
     if request.method == 'POST':
         msg = request.form['msg'].strip()
-        formuser = request.form['username'].strip()
-        formpass = request.form['password'].strip()
 
         if msg != "":
             conn = sqlite3.connect(DB_PATH)
             
-            
-            db_user = conn.execute("SELECT username FROM users WHERE username = ?", (formuser,)).fetchone()
-            
-            if db_user is None:
-                conn.execute(
-                    "INSERT INTO users (username,password) VALUES (?,?)",
-                    (formuser,formpass)
-                )
-                print("account created")
-                loggedIn =True
-            else:
-                print("user exists!")
-                db_pass = conn.execute("SELECT password FROM users WHERE username = ?", (formuser,)).fetchone()
-                
-                if formpass == db_pass[0]:
-                    loggedIn =True
-                else:
-                    sysmsg = "wrong pass"
-                    
-                if loggedIn:
-                     conn.execute(
-                        "INSERT INTO msgs (username,msg) VALUES (?,?)",
-                        (formuser,msg)
-                     )
+            formuser = session.get('username')
+            #if loggedIn:
+            conn.execute(
+                    "INSERT INTO msgs (username,msg) VALUES (?,?)",
+                    (formuser,msg)
+                 )
             conn.commit()
             conn.close()
 
@@ -74,7 +57,7 @@ def index():
     msgs = conn.execute("SELECT * FROM msgs").fetchall()
     conn.close()
 
-    return render_template('index.html', msgs=msgs, sysmsg=sysmsg, user=formuser, passed=formpass)
+    return render_template('index.html', msgs=msgs, sysmsg=sysmsg)
 
 @app.route('/database')
 def database():
@@ -82,7 +65,46 @@ def database():
     msgs = conn.execute("SELECT * FROM msgs").fetchall()
     users = conn.execute("SELECT * FROM users").fetchall()
     
-    return str(msgs) + "<br><br>" + str(users)
+    return render_template('database.html', msgs=msgs, users=users)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    sysmsg = ""
+    conn = sqlite3.connect(DB_PATH)
+    if request.method == 'POST':
+        formuser = request.form['username'].strip()
+        formpass = request.form['password'].strip()
+
+        db_user = conn.execute("SELECT username FROM users WHERE username = ?", (formuser,)).fetchone()
+
+        if db_user is None:
+            encryptedPass = generate_password_hash(formpass)
+            conn.execute(
+                "INSERT INTO users (username,password) VALUES (?,?)",
+                (formuser,encryptedPass)
+            )
+            print("account created")
+            loggedIn =True
+            sysmsg = "account created"
+        else:
+            print("user exists!")
+            db_pass = conn.execute("SELECT password FROM users WHERE username = ?", (formuser,)).fetchone()
+
+            if check_password_hash(db_pass[0], formpass):
+                loggedIn =True
+                sysmsg = "logged in"
+            else:
+                sysmsg = "wrong pass"
+                
+        conn.commit()
+        conn.close() 
+                
+        if loggedIn:
+            session['username'] = formuser
+            return redirect('/')
+                
+           
+    return render_template('login.html', sysmsg=sysmsg)
 
 if __name__ == "__main__":
     app.run()
